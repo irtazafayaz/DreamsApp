ea08ec9241c46ae82394899e8974520d88558903
Irtaza Fiaz <irtazab135@gmail.com>
2024-02-23 02:09:03 +0500

    Adding assets

--- DreamsApp/DreamsAppApp.swift
+++ DreamsApp/DreamsAppApp.swift
@@ -6,12 +6,50 @@
 //
 
 import SwiftUI
+import RevenueCat
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+       
+        Purchases.logLevel = .debug
+        Purchases.configure(withAPIKey: Constants.revenueCat)
+        Utilities.updateSubscription()
+        return true
+        
+    }
+}
 
 @main
 struct DreamsAppApp: App {
+    
+    @State private var showSplashScreen = true
+    @StateObject var userViewModel = UserViewModel()
+    @StateObject private var dataController = DataController()
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        UITableView.appearance().separatorStyle = .none
+        UITableView.appearance().tableFooterView = UIView()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if showSplashScreen {
+                SplashView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            showSplashScreen = false
+                        }
+                    }
+            } else {
+                ContentView()
+                    .environmentObject(userViewModel)
+                    .environment(\.managedObjectContext, dataController.container.viewContext)
+            }
         }
     }
 }
