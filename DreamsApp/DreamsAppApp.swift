94d43e4df54e78abdab07a40a0a961ce10020205
Irtaza Fiaz <irtaza@iiinigence.com>
2024-02-23 01:43:19 +0500

    Inital Project Setup

---
+++ DreamsApp/DreamsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamsAppApp.swift
+//  DreamsApp
+//
+//  Created by Irtaza Fiaz on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct DreamsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
